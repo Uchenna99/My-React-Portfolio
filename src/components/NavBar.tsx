f6e05fb3fa31e372b1b894f2ba5f84184b1cb470
Uchenna99 <uchennaagbu@gmail.com>
2025-01-04 20:30:55 +0100

    working on navbar functions

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,6 +8,8 @@ const NavBar = () => {
   const [work, setWork] = useState<boolean>(false);
   const [contact, setContact] = useState<boolean>(false);
 
+  const navState = [ home, about, skills, work, contact ]
+
   return (
     <>
       <div className="navbar-container">
@@ -16,24 +18,28 @@ const NavBar = () => {
           <div className="name-logo"> <h2>UCHE'S PORTFOLIO</h2> </div>
 
           <div className="nav-right">
-            <div className="nav-options">
+            <div className="nav-options" onClick={()=> setHome(true)}
+              style={{color: home? 'darkviolet' : ''}}
+              >
               <p id="p-reg">Home</p>
             </div>
-            <div className="nav-options">
+            <div className="nav-options" onClick={()=> setAbout(true)}>
               <p id="p-reg">About</p>
             </div>
-            <div className="nav-options">
+            <div className="nav-options" onClick={()=> setSkills(true)}>
               <p id="p-reg">Skills</p>
             </div>
-            <div className="nav-options">
+            <div className="nav-options" onClick={()=> setWork(true)}>
               <p id="p-reg">Work</p>
             </div>
-            <div className="nav-options">
+            <div className="nav-options" onClick={()=> setContact(true)}>
               <p id="p-reg">Contact</p>
             </div>
           </div>
 
-          <div className="underline"></div>
+          <div className="underline"
+            style={{right: home? '508px' : about? '434px' : skills? '361px' : work? '291px' : contact? '212px' : ''}}
+          ></div>
         </div>
 
         <div className="tablet-navbar"></div>
