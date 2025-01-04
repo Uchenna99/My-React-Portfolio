98a1ee2e2bd645f6606524c407da5a79a534ec13
Uchenna99 <uchennaagbu@gmail.com>
2025-01-04 18:53:49 +0100

    working on navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import "./stylesheet.css"
 import HomePage from './HomePage'
 
 function App() {
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,42 @@
+import { useState } from "react"
 
 
 const NavBar = () => {
+  const [home, setHome] = useState<boolean>(true);
+  const [about, setAbout] = useState<boolean>(false);
+  const [skills, setSkills] = useState<boolean>(false);
+  const [work, setWork] = useState<boolean>(false);
+  const [contact, setContact] = useState<boolean>(false);
+
   return (
-    <div>NavBar</div>
+    <>
+      <div className="navbar-container">
+
+        <div className="laptop-navbar">
+          <div className="name-logo"> <h2>UCHE'S PORTFOLIO</h2> </div>
+
+          <div className="nav-right">
+            <div className="nav-options">
+              <p id="p-reg">Home</p>
+            </div>
+            <div className="nav-options">
+              <p id="p-reg">About</p>
+            </div>
+            <div className="nav-options">
+              <p id="p-reg">Skills</p>
+            </div>
+            <div className="nav-options">
+              <p id="p-reg">Work</p>
+            </div>
+            <div className="nav-options">
+              <p id="p-reg">Contact</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="tablet-navbar"></div>
+      </div>
+    </>
   )
 }
 
