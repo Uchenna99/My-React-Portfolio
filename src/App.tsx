18351bef953eb245643374f78f399b528c4f6fd5
Uchenna99 <uchennaagbu@gmail.com>
2025-01-04 17:11:01 +0100

    setting up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './HomePage'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' />
+        <Route path='/' element={<HomePage/>} />
       </Routes>
     </>
   )
---
+++ src/HomePage.tsx
@@ -0,0 +1,21 @@
+import Footer from "./components/Footer"
+import NavBar from "./components/NavBar"
+
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="portfolio-wrap">
+
+        <NavBar />
+
+        <div className="hero"></div>
+
+        <Footer />
+
+      </div>
+    </>
+  )
+}
+
+export default HomePage
