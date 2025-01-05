65be0594a72dd6725b935f479f33d36a89172480
Uchenna99 <uchennaagbu@gmail.com>
2025-01-05 19:55:52 +0100

    hero started

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,12 @@ const Hero = () => {
         <div className="hero">
 
           <div className="hero-text-section">
-            <h2>Hello</h2>
+            <h2 id="h1">Hello,</h2>
+            <h2 id="h2">My name is UCHE</h2>
+            <h2>I am a Full Stack</h2>
+            <h2>Web Developer</h2>
+
+            <button className="hero-butn">Contact Me</button>
           </div>
 
           <div className="hero-image-section"></div>
