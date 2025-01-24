4824409ad46464933abe85b3a427a1bf8079fbc6
Uchenna99 <uchennaagbu@gmail.com>
2025-01-24 12:56:48 +0100

    removed loading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,36 +1,35 @@
 import { useInView } from "react-intersection-observer"
 import { useNavbarContext } from "../context/ContextProvider";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import me from "../assets/IMAGES/photos/Me.jpg"
-import axios from "axios";
 
-interface DataProps{
-  id: number;
-  imgName: string;
-  imgUrl: string;
-  createdAt: any;
-  updatedAt: any;
-}
+// interface DataProps{
+//   id: number;
+//   imgName: string;
+//   imgUrl: string;
+//   createdAt: any;
+//   updatedAt: any;
+// }
 
 const Hero = () => {
-  const getUrl = 'http://localhost:2006/myapi/v1/images';
+  // const getUrl = 'http://localhost:2006/myapi/v1/images';
   const {home, setHome, setAbout, setSkills, setContact, setWork, heroRef, contactRef, navPress} = useNavbarContext();
   const {ref, inView} = useInView({threshold: 0.5});
 
-  const [images, setimages] = useState<DataProps[] | null>(null);
-  const [imgLoading, setImgLoading] = useState(true);
+  // const [images, setimages] = useState<DataProps[] | null>(null);
+  // const [imgLoading, setImgLoading] = useState(true);
 
-  useEffect(()=>{
-    const getImages = async ()=>{
-      await axios.get<DataProps[]>(getUrl)
-      .then(response => setimages(response.data))
-      .catch(error => console.log(error)
+  // useEffect(()=>{
+  //   const getImages = async ()=>{
+  //     await axios.get<DataProps[]>(getUrl)
+  //     .then(response => {setimages(response.data); console.log(response.data)})
+  //     .catch(error => console.log(error)
       
-      )
-      setImgLoading(false);
-    };
-    getImages();
-  }, []);
+  //     )
+  //     setImgLoading(false);
+  //   };
+  //   getImages();
+  // }, []);
 
 
   useEffect(()=>{
@@ -65,10 +64,11 @@ const Hero = () => {
 
             <div className="hero-image-section">
               <div className="circle">
-                {
+                <img src={me} alt="" />
+                {/* {
                   imgLoading? <img src={me} alt="" style={{rotate:'45deg'}} /> :
                   <img src={images![0].imgUrl} alt="" />
-                }
+                } */}
               </div>
             </div>
             
