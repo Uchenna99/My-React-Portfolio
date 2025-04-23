f881658a0771c9210fa149b1ca15193e91854a0c
Uchenna99 <uchennaagbu@gmail.com>
2025-04-23 14:41:12 +0100

    working on work section

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -29,49 +29,13 @@ const Work = () => {
 
           <div className="work" ref={workRef}>
 
-            <div className="work-sample">
-              <div className="thumbnail">
-                <img src={bitmama} alt="work_thumbnail"
-                  style={{width:'100%'}}
-                />
-              </div>
-              <div className="desc-text">
-                <h4>Bitmama</h4>
-                <p>A clone of Bitmama.io home page and blog page(2024)</p>
-                <Link id="b-link" target="_blank" to={'https://bitmama-clone-ts.vercel.app/'}>
-                  <button id="thumb-butn">Visit</button>
-                </Link>
-              </div>
+            <div className="work-sample" style={{backgroundImage:`url(${bitmama})`}}>
+
             </div>
 
-            <div className="work-sample">
-              <div className="thumbnail">
-                <img src={nvidia} alt="work_thumbnail"
-                  style={{width:'100%'}}
-                />
-              </div>
-              <div className="desc-text">
-                <h4>NVIDIA</h4>
-                <p>A clone of NVIDIA home page(2024)</p>
-                <Link id="b-link" target="_blank" to={'https://nvidia-clone-ts.vercel.app/'}>
-                  <button id="thumb-butn">Visit</button>
-                </Link>
-              </div>
-            </div>
+            <div className="work-sample" style={{backgroundImage:`url(${nvidia})`}}></div>
 
-            <div className="work-sample">
-              <div className="thumbnail">
-                <img src={airbnb} alt="work_thumbnail"
-                  style={{width:'100%'}}
-                />
-              </div>
-              <div className="desc-text">
-                <h4>Airbnb</h4>
-                <p>A clone of Airbnb home page (2024)</p>
-                <Link id="b-link" target="_blank" to={'https://air-bnb-orpin.vercel.app/'}>
-                  <button id="thumb-butn">Visit</button>
-                </Link>
-              </div>
+            <div className="work-sample" style={{backgroundImage:`url(${airbnb})`}}>
             </div>
           </div>
 
