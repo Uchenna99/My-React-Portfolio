95ebae437e37768f2eff210bf71b952ae0875fc0
Uchenna99 <uchennaagbu@gmail.com>
2025-04-24 17:38:23 +0100

    working on about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -31,8 +31,14 @@ const About = ({  }: Props) => {
 
           <div className="about" ref={aboutRef}>
 
-            <div className="photo">
-              <img src={me} alt="photo" />
+            <div className="photo-info-wrap">
+
+              <div className="photo">
+                <img src={me} alt="photo" />
+              </div>
+
+              <div className="about-info"></div>
+
             </div>
             
             <div className="about-text-wrap">
@@ -40,17 +46,19 @@ const About = ({  }: Props) => {
                 <p>
                   Hi, I'm Uchenna Agbu, a passionate Full Stack Web Developer with a strong drive for building scalable, user-friendly 
                   applications. With a solid foundation in both front-end and back-end technologies, I specialize in crafting seamless 
-                  digital experiences that blend clean code with great design. From building responsive web interfaces to developing 
-                  robust APIs, I enjoy turning complex problems into elegant solutions. I'm experienced with modern tech stacks like 
-                  React, Node.js, TypeScript, Express, and PostgreSQL, and I thrive in environments that encourage continuous learning 
-                  and collaboration. Whether I'm working on a solo project or contributing to a team, I bring a proactive mindset, 
-                  attention to detail, and a genuine enthusiasm for development. I'm currently seeking opportunities where I can grow, 
+                  digital experiences that blend clean code with great design. <br />
+                  I'm experienced with modern tech stacks like React, Node.js, TypeScript, Express, and PostgreSQL, and I thrive in 
+                  environments that encourage continuous learning and collaboration. Whether I'm working on a solo project or 
+                  contributing to a team, I bring a proactive mindset, 
+                  attention to detail, and a genuine enthusiasm for development. <br />
+                  I'm currently seeking opportunities where I can grow, 
                   contribute meaningfully, and help create impactful software products. When I'm not coding, you'll find me exploring 
                   new tech trends, mentoring others, or enjoying a good book or game.
                   Let's build something great together.
                 </p>
               </div>
             </div>
+
           </div>
 
         </div>
