73b2ecb4036e1fc28e48061dbabb6672b25dfa80
Uchenna99 <uchennaagbu@gmail.com>
2025-06-15 01:49:03 +0100

    retouching hero section

--- src/components/Navbar2.tsx
+++ src/components/Navbar2.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavbarContext } from "../context/ContextProvider";
 import { Menu, X } from "lucide-react"
 
@@ -7,10 +7,28 @@ const Navbar2 = () => {
     const { currentSection, setCurrentSection, setScrolling, aboutRef, heroRef, skillsRef, workRef, contactRef } = useNavbarContext();
     
     const [burger, setBurger] = useState('hide');
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        const scrollPosition = window.scrollY;
+        const triggerPoint = 740; // px from top
+
+        if (scrollPosition > triggerPoint) {
+          setScrolled(true);
+        } else {
+          setScrolled(false);
+        }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
   return (
     <>
-      <div className="fixed top-0 left-0 z-50 w-full h-[60px] flex justify-center px-4 " 
+      <div className="fixed top-0 left-0 z-50 w-full h-[60px] flex justify-center px-4 transition-all duration-800 " 
+        style={{backgroundColor: scrolled? 'white':''}}
         onClick={()=>{burger==='show'? setBurger('hide'): null}}>
 
         <div className="w-full lg:w-[1200px] h-full flex justify-end sm:justify-center items-center relative ">
@@ -21,35 +39,35 @@ const Navbar2 = () => {
             <div className="nav-options" onClick={()=> { setCurrentSection("home");
               heroRef?.current?.scrollIntoView({behavior:'smooth'}) }}>
               <p className="text-slate-200 transition-all duration-200 " 
-                style={{fontFamily: currentSection==='home'? 'mbold':'', color: currentSection==='home'? '#00bc7d' : ''}}
+                style={{fontFamily: currentSection==='home'? 'mbold':'', color: currentSection==='home'? '#00bc7d': currentSection !== 'home' && scrolled? '#0f172b' : ''}}
               >Home</p>
             </div>
             
             <div className="nav-options" onClick={()=> { setCurrentSection("about"); 
               aboutRef?.current?.scrollIntoView({behavior:'smooth'}) }}>
               <p className="text-slate-200 transition-all duration-200 " 
-                style={{fontFamily: currentSection==='about'? 'mbold':'', color: currentSection==='about'? '#00bc7d' : ''}}
+                style={{fontFamily: currentSection==='about'? 'mbold':'', color: currentSection==='about'? '#00bc7d' : currentSection !== 'about' && scrolled? '#0f172b':''}}
               >About</p>
             </div>
 
             <div className="nav-options" onClick={()=> { setCurrentSection("skills"); 
               skillsRef?.current?.scrollIntoView({behavior:'smooth'}) }}>
               <p className="text-slate-200 transition-all duration-200 " 
-                style={{fontFamily: currentSection==='skills'? 'mbold':'', color: currentSection==='skills'? '#00bc7d' : ''}}
+                style={{fontFamily: currentSection==='skills'? 'mbold':'', color: currentSection==='skills'? '#00bc7d' : currentSection !== 'skills' && scrolled? '#0f172b' : ''}}
               >Skills</p>
             </div>
 
             <div className="nav-options" onClick={()=> { setCurrentSection("work"); 
               workRef?.current?.scrollIntoView({behavior:'smooth'}) }}>
               <p className="text-slate-200 transition-all duration-200 " 
-                style={{fontFamily: currentSection==='work'? 'mbold':'', color: currentSection==='work'? '#00bc7d' : ''}}
+                style={{fontFamily: currentSection==='work'? 'mbold':'', color: currentSection==='work'? '#00bc7d' : currentSection !== 'work' && scrolled? '#0f172b' : ''}}
               >Projects</p>
             </div>
             
             <div className="nav-options" onClick={()=> { setCurrentSection("contact");
               contactRef?.current?.scrollIntoView({behavior:'smooth'}) }}>
               <p className="text-slate-200 transition-all duration-200 " 
-              style={{fontFamily: currentSection==='contact'? 'mbold':'', color: currentSection==='contact'? '#00bc7d' : ''}}>Contact</p>
+              style={{fontFamily: currentSection==='contact'? 'mbold':'', color: currentSection==='contact'? '#00bc7d' : currentSection !== 'contact' && scrolled? '#0f172b' : ''}}>Contact</p>
             </div>
 
 
