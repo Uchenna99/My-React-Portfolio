47f1d066038aab2a98bdbefe148d845ca3f0756a
Uchenna99 <uchennaagbu@gmail.com>
2025-11-05 23:45:40 +0100

    changing hero

--- src/components/Hero2.tsx
+++ src/components/Hero2.tsx
@@ -19,19 +19,25 @@ const Hero2 = () => {
 
   return (
     <>
-    <div className="w-full md:h-[800px] flex justify-center px-4 md:px-10 "
+    <div className="w-full flex justify-center px-4 md:px-10 "
       style={{background: 'linear-gradient(to top, #1d293d 0%, #0f172b 60%, #0f172b 100%)'}}>
 
-      <motion.div className="relative h-full w-[1200px] max-w-full bg-blend-darken flex flex-col-reverse items-center md:items-end 
+      <motion.div className="relative h-full w-[1200px] max-w-full bg-blend-darken flex flex-col-reverse md:flex-row justify-between items-center md:items-end 
       -between py-20 gap-5 overflow-hidden " ref={heroRef}
         initial={{opacity:0, scale:1.1}} animate={{opacity:1, scale:1}} transition={{duration:0.5, ease:'easeOut'}}>
 
-        <motion.div className="flex flex-col gap-6 z-20" ref={ref}
-          initial={{opacity:0, y:80}} animate={{opacity:1, y:0}} transition={{duration:1, delay:0.3, ease:'easeOut'}} >
+        <motion.div className="flex flex-col gap-6 z-20 md:pb-20" ref={ref}
+          initial={{opacity:0}} animate={{opacity:1}} transition={{duration:1, delay:0.3, ease:'easeOut'}} >
 
-          <p className="text-slate-200 text-4xl md:text-5xl font-semibold ">Uchenna Agbu</p>
+          <motion.p className="text-slate-200 text-4xl md:text-5xl text-center font-semibold "
+            initial={{opacity:0, x:100}} animate={{opacity:1, x:0}} transition={{duration:0.8, delay:0.3, ease:'easeOut'}}>
+            Uchenna Agbu
+          </motion.p>
 
-          <p className="text-slate-200 text-lg md:text-xl text-center font-semibold">Full Stack Web Developer</p>
+          <motion.p className="text-slate-200 text-lg md:text-xl text-center font-semibold ital"
+            initial={{opacity:0, x:100}} animate={{opacity:1, x:0}} transition={{duration:0.8, delay:0.6, ease:'easeOut'}}>
+            Full Stack Web Developer
+          </motion.p>
 
           <div className="w-full flex flex-col sm:flex-row items-center gap-0 sm:gap-6">
             <button className="bg-emerald-500 hover:bg-emerald-700 w-fit h-12 rounded-lg text-xl text-slate-900 px-5 
@@ -49,7 +55,7 @@ const Hero2 = () => {
         </motion.div>
 
         
-        <div className="w-[500px] h-[650px] rounded-full border-2 border-white bg-cover bg-center bg-no-repeat"
+        <div className="w-[500px] aspect-[1/1.4] max-w-full rounded-full border-2 border-white bg-cover bg-center bg-no-repeat"
           style={{backgroundImage:`url(${imageUrl})`}}>
           {/* <img src={imageUrl} alt="My Photo" className="absolute right-[-240px] md:right-[-100px] bottom-[-120px] w-[900px] 
             min-w-[900px] opacity-40 "
