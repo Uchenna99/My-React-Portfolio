e685bb4ee844f288ff460d9c04efb0bb5b1a3163
Uchenna99 <uchennaagbu@gmail.com>
2025-11-05 23:30:06 +0100

    changing hero

--- src/components/Hero2.tsx
+++ src/components/Hero2.tsx
@@ -5,7 +5,8 @@ import { motion } from "framer-motion";
 
 
 const Hero2 = () => {
-  const imageUrl = "https://res.cloudinary.com/df6xz7bqp/image/upload/v1749919456/me_no_bg_crop_gwa5wr.png"
+  // const imageUrl = "https://res.cloudinary.com/df6xz7bqp/image/upload/v1762378023/Generated_Image_November_05_2025_-_9_18PM-min_i919fw.png"
+  const imageUrl = "https://res.cloudinary.com/df6xz7bqp/image/upload/v1762380159/Adobe_Express_-_file_3_-min_yeaq8b.png"
 
   const {scrolling, currentSection, setCurrentSection, heroRef, contactRef} = useNavbarContext();
   const {ref, inView} = useInView({threshold: 0.5});
@@ -18,27 +19,28 @@ const Hero2 = () => {
 
   return (
     <>
-    <div className="flex h-[800px] ">
-      <motion.div className="relative h-[800px] w-full bg-[0px_-370px] bg-cover bg bg-no-repeat bg-blend-darken
-        flex flex-col items-center justify-center px-4 md:px-10 overflow-hidden " ref={heroRef}
-        style={{background: 'linear-gradient(to top, #1d293d 0%, #0f172b 35%, #0f172b 100%)'}}
+    <div className="w-full md:h-[800px] flex justify-center px-4 md:px-10 "
+      style={{background: 'linear-gradient(to top, #1d293d 0%, #0f172b 60%, #0f172b 100%)'}}>
+
+      <motion.div className="relative h-full w-[1200px] max-w-full bg-blend-darken flex flex-col-reverse items-center md:items-end 
+      -between py-20 gap-5 overflow-hidden " ref={heroRef}
         initial={{opacity:0, scale:1.1}} animate={{opacity:1, scale:1}} transition={{duration:0.5, ease:'easeOut'}}>
 
-        <motion.div className="w-full lg:w-[1200px] flex flex-col gap-6 z-20 items-center md:items-start " ref={ref}
+        <motion.div className="flex flex-col gap-6 z-20" ref={ref}
           initial={{opacity:0, y:80}} animate={{opacity:1, y:0}} transition={{duration:1, delay:0.3, ease:'easeOut'}} >
 
-          <p className="text-slate-200 text-4xl md:text-5xl text-center font-semibold ">I'm Uchenna Agbu</p>
+          <p className="text-slate-200 text-4xl md:text-5xl font-semibold ">Uchenna Agbu</p>
 
           <p className="text-slate-200 text-lg md:text-xl text-center font-semibold">Full Stack Web Developer</p>
 
-          <div className="w-full flex items-center gap-10">
+          <div className="w-full flex flex-col sm:flex-row items-center gap-0 sm:gap-6">
             <button className="bg-emerald-500 hover:bg-emerald-700 w-fit h-12 rounded-lg text-xl text-slate-900 px-5 
-              font-semibold cursor-pointer transition-all duration-200 mt-7 "
+              font-semibold cursor-pointer transition-all duration-200 mt-7 text-nowrap"
               onClick={ ()=> contactRef?.current?.scrollIntoView({behavior:'smooth'}) }>
               Contact Me
             </button>
             <button className="bg-emerald-500 hover:bg-emerald-700 w-fit h-12 rounded-lg text-xl text-slate-900 px-5 
-              font-semibold cursor-pointer transition-all duration-200 mt-7 "
+              font-semibold cursor-pointer transition-all duration-200 mt-7 text-nowrap"
               onClick={ ()=> window.open('/Uchenna_Agbu_CV_.pdf', '_blank') }>
               View Resume
             </button>
@@ -47,10 +49,11 @@ const Hero2 = () => {
         </motion.div>
 
         
-        <div className="w-full lg:w-[1200px] absolute bottom-0 ">
-          <img src={imageUrl} alt="My Photo" className="absolute right-[-240px] md:right-[-100px] bottom-[-120px] w-[900px] 
+        <div className="w-[500px] h-[650px] rounded-full border-2 border-white bg-cover bg-center bg-no-repeat"
+          style={{backgroundImage:`url(${imageUrl})`}}>
+          {/* <img src={imageUrl} alt="My Photo" className="absolute right-[-240px] md:right-[-100px] bottom-[-120px] w-[900px] 
             min-w-[900px] opacity-40 "
-          />
+          /> */}
         </div>
 
       </motion.div>
