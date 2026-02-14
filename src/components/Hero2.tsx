c97b693700a4eca763c136e49d08c7af337b0ef2
Uchenna99 <uchennaagbu@gmail.com>
2026-02-14 21:37:47 +0100

    updating SEO

--- src/components/Hero2.tsx
+++ src/components/Hero2.tsx
@@ -22,33 +22,33 @@ const Hero2 = () => {
     <div className="w-full flex justify-center px-4 md:px-10 "
       style={{background: 'linear-gradient(to top, #1d293d 0%, #0f172b 60%, #0f172b 100%)'}}>
 
-      <div className="relative h-full w-[1200px] max-w-full bg-blend-darken flex flex-col-reverse md:flex-row justify-between items-center md:items-end 
-        py-20 gap-5 overflow-hidden " ref={heroRef}>
+      <div className="relative h-full w-[1200px] max-w-full bg-blend-darken flex flex-col-reverse md:flex-row justify-between items-center 
+        md:items-end py-20 gap-10 overflow-hidden " ref={heroRef}>
 
-        <motion.div className="flex flex-col gap-6 z-20 md:pb-20" ref={ref}
+        <motion.div className="w-[500px] max-w-full flex flex-col gap-6 z-20 md:pb-20" ref={ref}
           initial={{opacity:0}} animate={{opacity:1}} transition={{duration:1, delay:0.3, ease:'easeOut'}} >
 
-          <motion.p className="text-slate-200 text-4xl md:text-5xl text-center font-semibold "
+          <motion.p className="text-slate-300 text-3xl md:text-4xl max-md:text-center font-medium "
             initial={{opacity:0, x:100}} animate={{opacity:1, x:0}} transition={{duration:0.8, delay:0.3, ease:'easeOut'}}>
             Uchenna Agbu
           </motion.p>
 
-          <motion.p className="text-slate-200 text-lg md:text-xl text-center font-semibold ital"
+          <motion.h1 className="text-slate-200 text-4xl md:text-5xl max-md:text-center font-semibold ital"
             initial={{opacity:0, x:100}} animate={{opacity:1, x:0}} transition={{duration:0.8, delay:0.6, ease:'easeOut'}}>
-            Full Stack Web Developer
+            Full Stack MERN & React Developer
+          </motion.h1>
+
+          <motion.p className="text-slate-200 text-lg md:text-xl max-md:text-center font-medium ital"
+            initial={{opacity:0, x:100}} animate={{opacity:1, x:0}} transition={{duration:0.8, delay:0.8, ease:'easeOut'}}>
+            I build scalable, high-performance web applications using React, Node.js, Express, and MongoDB / PostgreSQL.
           </motion.p>
 
-          <div className="w-full flex flex-col sm:flex-row items-center justify-center gap-0 sm:gap-6">
-            <button className="bg-emerald-500 hover:bg-emerald-700 w-fit h-12 rounded-lg text-xl text-slate-900 px-5 
-              font-semibold cursor-pointer transition-all duration-200 mt-7 text-nowrap"
+          <div className="w-full flex max-md:justify-center mt-7">
+            <button className="bg-emerald-500 hover:bg-emerald-700 w-fit h-12 rounded-md text-xl text-slate-900 px-5 
+              font-semibold cursor-pointer transition-all duration-200 text-nowrap"
               onClick={ ()=> contactRef?.current?.scrollIntoView({behavior:'smooth'}) }>
               Contact Me
             </button>
-            {/* <button className="bg-emerald-500 hover:bg-emerald-700 w-fit h-12 rounded-lg text-xl text-slate-900 px-5 
-              font-semibold cursor-pointer transition-all duration-200 mt-7 text-nowrap"
-              onClick={ ()=> window.open('/Uchenna_Agbu_CV.pdf', '_blank') }>
-              View Resume
-            </button> */}
           </div>
 
         </motion.div>
@@ -57,7 +57,7 @@ const Hero2 = () => {
         <img
           src={my_image}
           alt="Uchenna Agbu"
-          className="w-[500px] aspect-[1/1.4] max-w-full rounded-full border-2 border-white object-cover"
+          className="w-[400px] md:w-[450px] aspect-[1/1.3] max-w-full rounded-full border-2 border-white object-cover"
           fetchPriority="high"
           loading="eager"
         />
